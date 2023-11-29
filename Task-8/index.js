a52e6b65d02051d550ed491c33c435a6acd30245
Gabriel Oliveira <gabriel.oliguima@gmail.com>
2023-11-29 14:17:14 -0300

    tarefa 8 feita

---
+++ Task-8/index.js
@@ -0,0 +1,13 @@
+//C: comprimento do círculo
+//A: área do círculo
+//R: raio do círculo
+
+const raio = 4;
+const pi = Math.PI;
+const comprimento = 2 * pi * raio;
+
+console.log(`O comprimento do círculo é ${comprimento}`);
+
+const areaCirculo = pi * Math.pow(raio, 2);
+
+console.log(`A área do círculo é ${areaCirculo}`);
