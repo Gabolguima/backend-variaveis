7b006465c10d704aca2d050a729e2170981f33e8
Gabriel Oliveira <gabriel.oliguima@gmail.com>
2023-11-29 15:35:39 -0300

    último exercício feito

---
+++ Task-13/index.js
@@ -0,0 +1,12 @@
+
+//At: área total do cilindro
+//R: raio da base
+//H: altura do cilindro
+
+const raioDaBase = 3;
+const alturaDoCilindro = 1;
+const pi = Math.PI;
+
+const areaTotal = 2 * pi * raioDaBase * (raioDaBase + alturaDoCilindro);
+
+console.log(areaTotal);
