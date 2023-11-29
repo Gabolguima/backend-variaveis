3ab5f869dc1e20342ed73d1ad698f281bba1eb57
Gabriel Oliveira <gabriel.oliguima@gmail.com>
2023-11-29 13:23:02 -0300

    exercício 5 feito

---
+++ Task-5/index.js
@@ -0,0 +1,9 @@
+const x1 = 1, x2 = 1;
+const y1 = 1, y2 = 4;
+
+// const potencia1 = (1 - 1) ** 2;
+// const potencia2 = (4 - 1) ** 2;
+
+const d = (x2 - x1) ** 2 + (y2 - y1) ** 2;
+
+console.log(Math.sqrt(d));
