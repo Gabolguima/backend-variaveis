25042ab60bfdb66dcde6a3ad1db36396279427d9
Gabriel Oliveira <gabriel.oliguima@gmail.com>
2023-11-29 13:46:39 -0300

    exercicio 6 feito

---
+++ Task-6/index.js
@@ -0,0 +1,7 @@
+const distancia = 500;
+const tempo = 10;
+
+const velocidadeEmKmh = distancia / tempo;
+const velocidadeEmMps = velocidadeEmKmh * 3.6;
+
+console.log(`A distância percorrida foi de ${velocidadeEmMps} km/h`);
